dfce30247aef60cdd3dce296cf5406693754e7f6
Danny (vai) San <vai.quoc.san@gmail.com>
2020-02-13 08:34:48 -0800

    rockPaperScissors.js(completed)

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -18,8 +18,48 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
+var rockPaperScissors = function (num) {
+  var rps = ['R', 'P', 'S']
+  var outcome = []
+//   var count = 0
+//   var keyCount = 0
+
+  for(var i = 0; i < rps.length; i++){
+    for(var j = 0; j < rps.length; j++){
+      for(var k = 0; k < rps.length; k++){
+        outcome.push(rps[i] + rps[j] + rps[k])
+      }
+    }
+  }
+
+
+
+  return outcome
   // TODO: your solution here
 };
 
+
+// var rockPaperScissors = function (num) {
+//   var rps = ['R', 'P', 'S']
+//   var outcome = {}
+//   var count = 0
+
+//   if(count <= num){
+//     for(var i = 0; i < rps.length; i++){
+//       if(outcome[count] === undefined){
+//         outcome[count] = rps[i]
+//       } else {
+//         outcome[count].concat(rps[i])
+//       }
+//     }
+//     count++
+//     rockPaperScissors(num - 1)
+//   }
+
+
+
+//   return Object.values(outcome)
+//   // TODO: your solution here
+// };
+
+// var testing = rockPaperScissors(num)
