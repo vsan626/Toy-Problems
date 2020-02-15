92643e448fc39970a17b278cb0fa6fe7325593f9
Danny (vai) San <vai.quoc.san@gmail.com>
2020-02-15 07:59:32 -0800

    constantTimeStackMin.js(completed)

--- constantTimeStackMin/constantTimeStackMin.js
+++ constantTimeStackMin/constantTimeStackMin.js
@@ -17,24 +17,62 @@
 /**
   * Stack Class
   */
-  var Stack = function() {
-
+var Stack = function () {
+  this.count = 0;
+  this.minCount = 0
+  this.storage = {};
+  this.minStorage = {};
+}
   // add an item to the top of the stack
-    this.push = function(value) {
-    };
+  Stack.prototype.push = function (value) {
+    if (this.minStorage[this.minCount] === undefined) {
+      this.minStorage[this.minCount] = value
+      this.minCount++
+    }
+    if (value <= this.minStorage[this.minCount]) {
+      this.minStorage[this.minCount] = value
+    }
+    this.storage[this.count] = value
+    this.count++
+    return this.storage[this.count - 1];
+  };
 
   // remove an item from the top of the stack
-    this.pop = function() {
-    };
-
-  // return the number of items in the stack
-    this.size = function() {
-    };
-  
-  // return the minimum value in the stack
-    this.min = function() {
-
-    };
+  Stack.prototype.pop = function () {
+    delete this.storage[this.count]
+    delete this.minStorage[this.minCount]
+    if (this.count && this.minCount > 0) {
+      this.count--
+      this.minCount--
+    }
 
   };
 
+  // return the number of items in the stack
+  Stack.prototype.size = function () {
+    return this.count
+  };
+
+  // return the minimum value in the stack
+  Stack.prototype.min = function () {
+    return this.minStorage[this.minCount - 1]
+  };
+
+
+debugger
+var example = new Stack()
+example.push(4)
+console.log(example.push(4))
+example.push(3)
+console.log(example.push(3))
+example.min() // === 3
+console.log(example.min())
+example.push(3)
+console.log(example.push(3))
+example.push(2)
+console.log(example.push(2))
+example.push(2)
+console.log(example.push(2))
+
+example.min() //=== 2
+console.log(example.min())
