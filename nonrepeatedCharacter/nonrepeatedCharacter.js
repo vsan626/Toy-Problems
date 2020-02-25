da9b210178d3afa4d85ebafbf05ff6dd1c67e318
Danny (vai) San <vai.quoc.san@gmail.com>
2020-02-24 21:02:40 -0800

    nonrepeatedCharacter completed

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -8,4 +8,25 @@
 
 var firstNonRepeatedCharacter = function(string) {
   // TODO: your solution here
+  let repeatCounter = {}
+  let strArr = string.split('')
+  let result;
+  for(let i = 0; i < strArr.length; i++){
+    if(repeatCounter[strArr[i]] === undefined){
+      repeatCounter[strArr[i]] = 0
+    } else {
+      repeatCounter[strArr[i]]++
+    }
+  }
+  for(let key in repeatCounter){
+    if (repeatCounter[key] === 0){
+      result =  key
+      break
+    }
+  }
+  return result
+  // console.log(repeatCounter)
 };
+
+debugger
+firstNonRepeatedCharacter('AACBDB')
